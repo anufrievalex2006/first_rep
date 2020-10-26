490d558630984a12bdea1d56dbe43f948b8ed331
anufrievalex2006 <73475465+anufrievalex2006@users.noreply.github.com>
2020-10-26 20:39:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VS_code
+{
+    class Program
+    {
+        static int[] s;
+        static Random r = new Random();
+        static int b, max;
+        static void Main(string[] args)
+        {
+            s = new int[100];
+            b = s.Length;
+            for (int i = 0; i < b; i++)
+            {
+                s[i] = r.Next(-100, 100);
+                Console.WriteLine("Элемент №{0}: значение {1}.", i + 1, s[i]);
+            }
+            max = s[1];
+            for (int j = 2; j < b; j++)
+            {
+                if (s[j] > max)
+                {
+                    max = s[j];
+                }
+            }
+            for (int a = 0; a < b; a++)
+            {
+                if (max == s[a])
+                {
+                    Console.WriteLine("Максимальный элемент №{0} массива со значением {1}", a + 1, max);
+                }
+            }
+			Console.ReadKey();
+        }
+    }
+}
